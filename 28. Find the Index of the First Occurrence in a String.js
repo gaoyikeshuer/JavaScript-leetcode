0387932e046569d1eeb67383738591a8c809eecd
Yike Gao <83198150+gaoyikeshuer@users.noreply.github.com>
2022-12-15 19:30:48 +0000

    Create 28. Find the Index of the First Occurrence in a String.js
    Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
    
     
    
    Example 1:
    
    Input: haystack = "sadbutsad", needle = "sad"
    Output: 0
    Explanation: "sad" occurs at index 0 and 6.
    The first occurrence is at index 0, so we return 0.
    Example 2:
    
    Input: haystack = "leetcode", needle = "leeto"
    Output: -1
    Explanation: "leeto" did not occur in "leetcode", so we return -1.

---
+++ 28. Find the Index of the First Occurrence in a String.js
@@ -0,0 +1,6 @@
+var strStr = function(haystack, needle) {
+    const startIndex = haystack.indexOf(needle)
+    return startIndex
+};
+
+// It seems too simple to solve. I guess I definitely miss something.
